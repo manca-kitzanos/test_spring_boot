70435505ba1a1b3325158a8948d019e6e2e502a8
Antonio Manca <manca@kitzanos.com>
2024-10-25 12:35:38 +0200

    Terzo Test

--- src/main/java/Bflows/test_spring_boot/TestSpringBootApplication.java
+++ src/main/java/Bflows/test_spring_boot/TestSpringBootApplication.java
@@ -8,7 +8,7 @@ public class TestSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestSpringBootApplication.class, args);
-		System.out.println("Hello, World! New Test");
+		System.out.println("Hello, World! New New Test");
 	}
 
 }
